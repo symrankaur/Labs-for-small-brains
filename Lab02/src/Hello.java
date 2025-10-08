a7e25d2d41ae0ca83ffdd605a2ff59c0f497ccf6
simran <simran@mac.attlocal.net>
2025-10-07 21:08:44 -0700

    line8 updated

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -1,4 +1,21 @@
 
+
 public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		System.out.println("Simrankaur18 - This line was added by Simrandeep Kaur. ");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
+}
 
 }
