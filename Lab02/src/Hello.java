047bec79d864bca902a75a6d510f3b7a17de771e
Amy Yang <159972697+amoogy@users.noreply.github.com>
2025-10-08 16:30:11 -0700

    changed line 9

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		System.out.println("Simrankaur18 - This line was added by Simrandeep Kaur. ");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("ayang17 - This line was added by Amy Yang.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
