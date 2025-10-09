3224c82dc00ac0729eb195e91397ee24ab540a82
Khush <khushpreetkaur2@csus.edu>
2025-10-09 13:39:20 -0700

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("Simrankaur18 - This line was added by Simrandeep Kaur. ");
 		System.out.println("ayang17 - This line was added by Amy Yang.");
 		System.out.println("Ssandhu6 - This line was added by Sahib Sandhu.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("Khushpreetkaur2 - This line was added by Khushpreet Kaur.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
